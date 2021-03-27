087779528603ff4efff34ce25145ad38c84b4ce3
jess <jlbachel@umich.edu>
2021-03-27 19:03:04 -0400

    fixed navigation & home page

--- src/Pages/Art/Art.tsx
+++ src/Pages/Art/Art.tsx
@@ -4,15 +4,6 @@ function Art() {
     return (
       <div id="container">
         <header> Art Page</header>
-        <p>art 1</p>
-        <p>art 2</p>
-        <p>art 3</p>
-        <p>art 4</p>
-        <p>art 5</p>
-        <p>art 6</p>
-        <p>art 7</p>
-        <p>art 8</p>
-        <p>art 9</p>
       </div>
     );
   }
