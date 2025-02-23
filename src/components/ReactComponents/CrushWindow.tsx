56343f5e6013a979f68ab77c32b553820b002c71
Jess <jlbachel@umich.edu>
2025-02-22 23:25:57 -0500

    solid styling occurring

--- src/components/ReactComponents/CrushWindow.tsx
+++ src/components/ReactComponents/CrushWindow.tsx
@@ -1,42 +1,57 @@
+import React from 'react';
 import './style.css'
-import { ReactComponent as MinimizeIcon }  from '../../assets/minimize-8.svg';
+// import { ReactComponent as MinimizeIcon }  from '../../assets/minimize-8.svg';
 
 type Props = {
     theme?: string;
     crushName?: string;
 }
 
-function CrushWindow(props: Props) {
+function CrushWindows(props: Props) {
     const {theme, crushName} = props
+    const [showWindow, setShowWindow] = React.useState(false)
+
+    const onSelect = () => {
+        console.log('ooo')
+    }
+
+    const testCrushes = [{crushName: "Crush A"}, {crushName: "Crush B"}, {crushName: "Crush C"}, {crushName: "Crush D"}]
+
 
     return (
-        
-        <div class="main-crush-window">
-            {/* <MinimizeIcon/> */}
-            <span class="main-crush-header">
-                <span class="crush-name">{crushName ? crushName : "Billy Bob"}</span>
-                {/* <MinimizeIcon/> */}
-                <span class="buttons">
-                    <button><span>_</span></button>
-                    <button><span>|_|</span></button>
-                    <button><span>x</span></button>
-                </span>
-            </span>
-            <hr class="crush-divider"/>
-            <div class="crush-picture"></div>
-            <hr class="crush-divider"/>
-            <div>
-                <button>
-                    approve
-                </button>
-                <button>
-                    veto
-                </button>
-                <button>
-                    show updates
-                </button>
-            </div>
+        <div id="crush-blocks">
+            {testCrushes.map((crush)=> {
+                return (
+                    <div className="main-crush-window">
+                    {/* <MinimizeIcon/> */}
+                    <span className="main-crush-header">
+                        <span className="crush-name">{crush.crushName ? crush.crushName : "Billy Bob"}</span>
+                        {/* <MinimizeIcon/> */}
+                        <span className="buttons">
+                            <button><span>_</span></button>
+                            <button><span>|_|</span></button>
+                            <button><span>x</span></button>
+                        </span>
+                    </span>
+                    <hr className="crush-divider"/>
+                    <div className="crush-picture"></div>
+                    <hr className="crush-divider"/>
+                    <div>
+                        <button>
+                            approve
+                        </button>
+                        <button>
+                            veto
+                        </button>
+                        <button onClick={onSelect}>
+                            show updates
+                        </button>
+                    </div>
+                    </div>
+   
+                )
+            })}
         </div>
     );
 }
-export default CrushWindow
+export default CrushWindows
